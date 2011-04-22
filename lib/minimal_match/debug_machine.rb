a9c0e02290b5cfd5b2445a074e420404df1b1bde
Stephen Prater (@dev-env) <stephenp@agrussell.com>
2011-04-22 17:05:23 -0500

    debug machine drawing

--- lib/minimal_match/debug_machine.rb
+++ lib/minimal_match/debug_machine.rb
@@ -72,20 +72,15 @@ module MinimalMatch
         end
       end,
       moveto: lambda do |*args|
-        row = nil, col = nil, opts = {}  # fill with args
-        debugger
-        row, col = { :row => nil, :col => nil}.merge(opts).values
+        # pass it either a [row,col] array or a row:x, col:x opts hash.
+        row, col = { :row => nil, :col => nil}.merge(args.first).values rescue args
         cmd = (col and row) ? 'cup' : (col ? 'hpa' : (row ? 'vpa' : 'home'))
         `tput #{cmd} #{col} #{row}`
       end,
-      current_position: lambda do |t|
-        # holy mother of what's the fucking point of u7??
-        system("stty -echo; tput u7; read -d R x; stty echo; echo ${x#??} > #{t.path}")
-        t.read.chomp.split(';').map(&:to_i)
-      end
     }
     def initialize(prog, subj, col = 1, zeroth = [nil,nil])
-      @temp_file = Tempfile.new('debug_machine')
+      system "mkfifo /tmp/debug_machine#{uniq_id}"
+      @temp_file = File.open("/tmp/debug_machine#{uniq_id}","w+")
       @delay = 0.25 
       @parent = true
       @program = prog
@@ -94,10 +89,17 @@ module MinimalMatch
       first_width = @subj.map(&:to_s).map(&:length).max + 10
       inst_width = @program.map(&:to_s).map(&:length).max + 10
       @width = [first_width, inst_width].max
-      unless zeroth.compact.empty?
+      
+      unless zeroth.compact.empty? then
         @home = zeroth
       end
+      
       _elements
+
+    end
+
+    def uniq_id 
+      @uniq_id ||= "#{"%x" % (Time.now.to_i + self.__id__).hash}".upcase
     end
 
     def _elements
@@ -115,6 +117,13 @@ module MinimalMatch
         str
       end.call
 
+      # so i can get to "@socket"
+      @commands[:current_position] = lambda do 
+        # holy mother of what's the fucking point of u7??
+        system("stty -echo; tput u7; read -d R x; stty echo; echo ${x#??} > #{@temp_file.path}")
+        @temp_file.gets.chomp.split(';').map(&:to_i)
+      end
+
       @commands[:line] = lambda do
         smacs { "#{'q' * @size[:cols]}" }
       end.call
@@ -123,28 +132,18 @@ module MinimalMatch
       @commands[:clear] = lambda do
         str = ""
         str << color(2) { @commands[:line] }
-        (@size[:cols] * @size[:rows]).times do
-          str << ' '
+        (@size[:rows]).times do
+          str << @commands[:mrcup][1,0]
+          str << `tput el`
         end
         str << color(2) { @commands [:line] }
         str << "\n"
         str
       end.call
 
-      @commands[:subject] = lambda do
-        str = "" 
-        str << color(15) { self.to_s + "\n" }
-        (@size[:rows]-1).times do |idx|
-          str << color(7) { " #{idx} : #{@subj[idx]} " } if @subj[idx]
-          str << @commands[:moveto][:col => (@width - 2)]
-          str << smacs{ "x\n" }
-        end
-        str
-      end.call
-
       @commands[:self] = lambda do
         str = ""
-        str << @commands[:mrcup][1,0] # avoid the top line
+        str << @commands[:mrcup][2,0] # avoid the top line
         @program.each_with_index do |inst,idx|
           str << @commands[:moveto][:col => (@col * @width)]
           str << color(3) { " --- " }
@@ -154,13 +153,26 @@ module MinimalMatch
         end
         str
       end.call
+
+      @commands[:subject] = lambda do
+        str = "" 
+        str << color(15) { self.to_s + "\n" }
+        (@size[:rows] - 1).times do |idx|
+          str << color(7) { " #{idx} : #{@subj[idx]} " } if @subj[idx]
+          str << @commands[:moveto][:col => (@width - 2)]
+          str << smacs{ "x\n" }
+        end
+        str
+      end.call
       true
     end
 
     def draw_subject(hilight = nil)
       str = ""
       if not hilight
-        str << @commands[:subject] 
+        str << @commands[:subject]
+        # end at the top
+        str << @commands[:mrcup][-(@commands[:subject].lines.count),0]
       else
         str << @commands[:moveto][:col => 1]
         str << @commands[:mrcup][hilight+1,0]
@@ -168,21 +180,21 @@ module MinimalMatch
         str << @commands[:moveto][:col => 1]
       end
       #end at the top
-      str << @commands[:mrcup][-@size[:rows]+2,0] # the lines, natch
       str
     end
 
     def draw_self(inst = nil)
       str = ""
       if not inst
-        str << @commands[:self] 
+        str << @commands[:self]
+        #end at the top
+        str << @commands[:mrcup][-(@commands[:self].lines.count),0]
       else
         str << @commands[:moveto][:col => @first_col]
         str << @commands[:mrcup][inst+1,0]
-        str << @commands[:moveto][:col => ((@col * @width) + @first_col)]
+        str << @commands[:moveto][:col => ((@col * @width))]
         str << color(1) { " >>>" }
       end
-      str << @commands[:mrcup][-@size[:rows]+2,0]
       str
     end
 
@@ -201,34 +213,39 @@ module MinimalMatch
       str
     end
     
-    def display inst = nil, subj = nil
+    def display
       str = ""
       if @home
         @commands[:moveto][*@home]
       end
       str << @commands[:clear] if @parent
-      str << @commands[:mrcup][-(@size[:rows]+2),0] #two lines
-      str << @commands[:moveto][:col => 1] # just for good measure
-      str << draw_subject(subj)
-      str << draw_self(inst) 
-      str << @commands[:mrcup][@size[:rows]+2,0] #to the end!
+      str << @commands[:mrcup][-(@size[:rows]+1),0] #two lines
+      str << @commands[:moveto][col:1] # just for good measure
+      str << draw_subject()
+      str << draw_self() 
+      str << @commands[:mrcup][@size[:rows]+3,0] #to the end!
       sleep 0.1
       $stdout.print str
-      @home = @commands[:current_position].call @temp_file
+      @home = @commands[:current_position][]
       @home
     end
 
-    def update inst, subj, opts = {} 
-      offby = opts[:offby]
-      goto = opts[:at]
+    def update subj,inst opts = {}
+      in_place = opts[:in_place] || opts[:inplace]
+      goto = opts[:goto] || opts[:at]
       # incase you're running this in irb or something, you can do offby: 2
       # and it will move up an extra two lines
-      if goto
-        str << @commands[:moveto][:row => goto[0], :col => goto[1]]
-      else
-        str << @commands[:mrcup][-(@size[:rows]+2+offby),0]
+      str = ""
+      display unless goto or in_place
+      str = there_and_back do
+        str << @commands[:moveto][*goto] if goto
+        str << @commands[:mrcup][*@home] if in_place
+        str << @commands[:mrcup][-(@size[:rows]+1),0] #top!
+        str << @commands[:moveto][col:1]
+        str << draw_subject(subj) if subj
+        str << draw_self(inst) if inst
       end
-      display(inst,subj)
+      $stdout.print str
       true
     end
 
