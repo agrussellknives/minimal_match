fc97ae70446b15bf9f2fb4598acd360f80c677a0
Stephen Prater (@dev-env) <stephenp@agrussell.com>
2011-04-26 17:02:16 -0500

    working on debugger

--- lib/minimal_match/debug_machine.rb
+++ lib/minimal_match/debug_machine.rb
@@ -22,70 +22,118 @@ module MinimalMatch
       end
     end
 
-    attr_accessor :parent
-    attr_accessor :delay
-    attr_accessor :commands #TODO rmove this
+  class DebugMachineCache
+
+    remove_const :COMMANDS rescue nil#TODO remove this
+
+    # assemble format strings for movement commands
+    movmts = Hash[[:hpa,:vpa,:cup,:home,:setaf].zip(
+      [`tput hpa`, `tput vpa`, `tput cup`,`tput home`].collect do |fmt|
+      fmt.split(/(%[pPg][\d\w])|(%.)/).each_with_object '' do |i,m|
+        if i == '%d'
+          m << '%d'
+        elsif i[0] != '%'
+          m << i
+        end
+      end
+    end)]
     
-    def color color
+    setaf = `tput setaf`.split(/(%.*?(?=%)|;)/)
+    setaf = "#{setaf[0]}%s#{setaf[-1]}"
+      
+    COMMANDS = {
+    save: `tput sc`,
+    restore: `tput rc`,
+    reset: `tput sgr0`,
+    smacs: `tput smacs`,
+    rmacs: `tput rmacs`,
+    left: `tput cub1`,
+    right: `tput cuf1`,
+    up: `tput cuu1`,
+    down: `tput cud1`,
+    test: 'test',
+    setaf: lambda do |color|
       str = ""
-      str << `tput setaf #{color}`
-      str << yield
-      str << @commands[:reset] 
+      if color < 8
+        str = "3#{color}"
+      elsif color < 16
+        str = "9#{color-8}"
+      elsif
+        str = "38;5;#{color}"
+      end
+      setaf % str 
+    end,
+    mrcup: lambda do |row,col|
+      hdir = col > 0 ? :right : :left
+      vdir = row > 0 ? :down : :up 
+      [[hdir,col],[vdir,row]].inject '' do |memo, dir|
+        memo << (COMMANDS[dir[0]] * dir[1].abs)
+      end
+    end,
+    moveto: lambda do |*args|
+      # pass it either a [row,col] array or a row:x, col:x opts hash
+      row,col = *({ :row => nil, :col => nil}.merge(args.first).values rescue args)
+      cmd = (col and row) ? :cup : (col ? :hpa : (row ? :vpa : :home))
+      movmts[cmd] % ([row, col].compact)
+    end
+    }
+    COMMANDS.each_pair do |meth,b|
+      c = b.is_a?(Proc) ? b : lambda { b }
+      define_method meth, &c
+    end
+  end
+
+    # abstractions around common escape sequences or construction
+    def output to_stdout = false
+      string = ""
+      yield(string) # modifiy str in place
+      if to_stdout
+        $stdout << string
+      end
+      string
+    end
+ 
+    def color color
+      output do |str|
+        str << c.setaf(color)
+        str << yield
+        str << c.reset
+      end
     end
 
     def there_and_back
       warn "nested there_and_back" if @within
       @within = true
-      str = ""
-      str << @commands[:save]
-      str << yield
-      str << @commands[:restore]
+      string = output do |str|
+        str << c.save 
+        str << yield
+        str << c.restore
+      end
       @within = false
-      str
+      string
     end
 
     def smacs
-      str = ""
-      str << @commands[:smacs]
-      str << yield
-      str << @commands[:rmacs]
-      str
+      output do |str|
+        str << c.smacs 
+        str << yield
+        str << c.rmacs 
+      end
     end
 
-    remove_const :COMMANDS rescue nil#TODO remove this
+    
+    attr_accessor :delay
+    attr_reader :home
+    attr_reader :parent
 
-    COMMANDS = {
-      save: `tput sc`,
-      restore: `tput rc`,
-      reset: `tput sgr0`,
-      smacs: `tput smacs`,
-      rmacs: `tput rmacs`,
-      left: `tput cub1`,
-      right: `tput cuf1`,
-      up: `tput cuu1`,
-      down: `tput cud1`,
-      mrcup: lambda do |row,col|
-        hdir = col > 0 ? :right : :left
-        vdir = row > 0 ? :down : :up 
-        [[hdir,col],[vdir,row]].inject'' do |memo, dir|
-          memo << (COMMANDS[dir[0]] * dir[1].abs)
-        end
-      end,
-      moveto: lambda do |*args|
-        # pass it either a [row,col] array or a row:x, col:x opts hash.
-        row, col = { :row => nil, :col => nil}.merge(args.first).values rescue args
-        cmd = (col and row) ? 'cup' : (col ? 'hpa' : (row ? 'vpa' : 'home'))
-        `tput #{cmd} #{col} #{row}`
-      end,
-    }
     def initialize(prog, subj, col = 1, zeroth = [nil,nil])
       system "mkfifo /tmp/debug_machine#{uniq_id}"
       @temp_file = File.open("/tmp/debug_machine#{uniq_id}","w+")
       @delay = 0.25 
       @parent = true
-      @program = prog
+      @program = prog.dup #duplicate the subject and instruction set 
+      @subj = subj.dup 
       @col = col
-      @subj = subj
       first_width = @subj.map(&:to_s).map(&:length).max + 10
       inst_width = @program.map(&:to_s).map(&:length).max + 10
       @width = [first_width, inst_width].max
@@ -94,163 +142,178 @@ module MinimalMatch
         @home = zeroth
       end
       
-      _elements
+    _cache
 
+    end
+
+    def c
+      @commands
     end
 
     def uniq_id 
       @uniq_id ||= "#{"%x" % (Time.now.to_i + self.__id__).hash}".upcase
     end
 
-    def _elements
-      
-      @commands = COMMANDS.dup
-      @size = { :cols => `tput cols`.to_i, :rows => @program.length + 1}
+    def _cache
+      @commands = DebugMachineCache.new
+      @size = {:cols => `tput cols`.to_i, :rows => @program.length + 1}
+      commands = {}
+      commands[:size] = @size
 
-      @commands[:info] = lambda do
-        str = ""
-        str << there_and_back do
-          str << @commands[:mrcup][@size[:rows] + 1,0]
-          str << @commands[:moveto][:col => 0]
+      # this is completley fucking wrong
+      commands[:info] = lambda do
+        output do |str|
+          str << there_and_back do
+            str << c.mrcup(@size[:rows] + 1,0)
+            str << c.moveto(col: 0)
+          end
+          str << '%s'
         end
-        str << '%s'
-        str
       end.call
 
-      # so i can get to "@socket"
-      @commands[:current_position] = lambda do 
-        # holy mother of what's the fucking point of u7??
-        system("stty -echo; tput u7; read -d R x; stty echo; echo ${x#??} > #{@temp_file.path}")
-        @temp_file.gets.chomp.split(';').map(&:to_i)
-      end
-
-      @commands[:line] = lambda do
+      commands[:line] = lambda do
         smacs { "#{'q' * @size[:cols]}" }
       end.call
 
       #redraw the display area, assume we're at the beggining
-      @commands[:clear] = lambda do
-        str = ""
-        str << color(2) { @commands[:line] }
-        (@size[:rows]).times do
-          str << @commands[:mrcup][1,0]
-          str << `tput el`
+      commands[:clear] = lambda do
+        output do |str|
+          str << color(2) { commands[:line] }
+          (@size[:rows]).times do
+            str << c.mrcup(1,0)
+            str << `tput el`
+          end
+          str << color(2) { commands[:line] } 
+          str << "\n"
         end
-        str << color(2) { @commands [:line] }
-        str << "\n"
-        str
       end.call
 
-      @commands[:self] = lambda do
-        str = ""
-        str << @commands[:mrcup][2,0] # avoid the top line
-        @program.each_with_index do |inst,idx|
-          str << @commands[:moveto][:col => (@col * @width)]
-          str << color(3) { " --- " }
-          str << color(7) { "#{idx}: #{inst}" }
-          str << @commands[:moveto][:col => ((@col + 1) * @width)]
-          str << smacs { "x\n" }
+      commands[:self] = lambda do
+        output do |str|
+          str << c.mrcup(2,0) # avoid the top line
+          @program.each_with_index do |inst,idx|
+            str << c.moveto(col: (@col * @width))
+            str << color(3) { " --- " }
+            str << color(7) { "#{idx}: #{inst}" }
+            str << c.moveto(col: ((@col + 1) * @width))
+            str << smacs { "x\n" }
+          end
         end
-        str
       end.call
 
-      @commands[:subject] = lambda do
-        str = "" 
-        str << color(15) { self.to_s + "\n" }
-        (@size[:rows] - 1).times do |idx|
-          str << color(7) { " #{idx} : #{@subj[idx]} " } if @subj[idx]
-          str << @commands[:moveto][:col => (@width - 2)]
-          str << smacs{ "x\n" }
+      commands[:subject] = lambda do
+        output do |str|
+          str << color(15) { self.to_s + "\n" }
+          str << output do |str|
+            (@size[:rows] - 1).times do |idx|
+              str << color(7) { " #{idx} : #{@subj[idx]} " } if @subj[idx]
+              str << c.moveto(col: (@width - 2))
+              str << smacs{ "x\n" }
+            end
+          end
         end
-        str
       end.call
-      true
+      
+      commands.each_pair do |meth,b|
+        c = b.is_a?(Proc) ? b : lambda { b }
+        @commands.define_singleton_method meth, &c
+      end
+
+      @commands
+    end
+
+    def current_position
+      system("stty -echo; tput u7; read -d R x; stty echo; echo ${x#??} > #{@temp_file.path}")
+      @temp_file.gets.chomp.split(';').map(&:to_i)
     end
 
     def draw_subject(hilight = nil)
-      str = ""
-      if not hilight
-        str << @commands[:subject]
-        # end at the top
-        str << @commands[:mrcup][-(@commands[:subject].lines.count),0]
-      else
-        str << @commands[:moveto][:col => 1]
-        str << @commands[:mrcup][hilight+1,0]
-        str << color(6) { "*#{hilight} : #{@subj[hilight]}*" }
-        str << @commands[:moveto][:col => 1]
+      output do |str|
+        str << c.subject 
+        str << c.mrcup(-(c.subject.lines.count),0)
       end
-      #end at the top
-      str
     end
 
-    def draw_self(inst = nil)
-      str = ""
-      if not inst
-        str << @commands[:self]
-        #end at the top
-        str << @commands[:mrcup][-(@commands[:self].lines.count),0]
-      else
-        str << @commands[:moveto][:col => @first_col]
-        str << @commands[:mrcup][inst+1,0]
-        str << @commands[:moveto][:col => ((@col * @width))]
-        str << color(1) { " >>>" }
+    def update_subject(hilight)
+      output do |str|
+        str << draw_subject
+        str << output do |str|
+          str << c.moveto(col: 1)
+          str << c.mrcup(hilight+1,0)
+          str << color(6) { "*#{hilight} : #{@subj[hilight]}*" }
+          str << c.moveto(col: 1)
+          str << c.mrcup(-(hilight+1),0)
+        end
       end
-      str
     end
 
+    def draw_self
+      output do |str|
+        str << c.self
+        str << c.mrcup(-(c.self.lines.count),0)
+      end
+    end
+
+    def update_self(inst)
+      output do |str|
+        str << draw_self
+        str << output do |str|
+          str << c.moveto(col: ((@col * @width)))
+          str << c.mrcup(inst+1,0)
+          str << c.moveto(col: ((@col * @width)))
+          str << color(1) { " >>> #{inst}: #{@program[inst]}" }
+          str << c.mrcup(-(inst+1),0)
+        end
+      end
+    end
+    
     def puts msg
-      $stdout << (@commands[:info] % msg)
+      $stdout << (c.info % msg)
     end
 
-    def run command, *args
-      x = @commands[command]
-      str = ""
-      if x.respond_to? :call
-        str << @commands[command][*args]
-      else
-        str << @commands[command]
-      end
-      str
+    def inspect
+      "#<#{self.class}:#{'0x%x' % self.__id__ << 1} @program=#{@program}> @delay=#{@delay}"
+    end
+
+    def to_s
+      "#<#{self.class}:#{'0x%x' % self.__id__ << 1}>"
     end
     
     def display
-      str = ""
-      if @home
-        @commands[:moveto][*@home]
+      output :to_stdout do |str|
+        str << c.clear if @parent
+        str << c.mrcup(-(@size[:rows]+1),0) #two lines
+        str << c.moveto(col:1) # just for good measure
+        str << draw_subject()
+        str << draw_self() 
+        str << c.mrcup(@size[:rows]+3,0) #to the end!
       end
-      str << @commands[:clear] if @parent
-      str << @commands[:mrcup][-(@size[:rows]+1),0] #two lines
-      str << @commands[:moveto][col:1] # just for good measure
-      str << draw_subject()
-      str << draw_self() 
-      str << @commands[:mrcup][@size[:rows]+3,0] #to the end!
-      sleep 0.1
-      $stdout.print str
-      @home = @commands[:current_position][]
-      @home
+      # save the home position returning it
+      @home = current_position 
     end
 
-    def update subj,inst opts = {}
-      in_place = opts[:in_place] || opts[:inplace]
-      goto = opts[:goto] || opts[:at]
-      # incase you're running this in irb or something, you can do offby: 2
-      # and it will move up an extra two lines
-      str = ""
-      display unless goto or in_place
-      str = there_and_back do
-        str << @commands[:moveto][*goto] if goto
-        str << @commands[:mrcup][*@home] if in_place
-        str << @commands[:mrcup][-(@size[:rows]+1),0] #top!
-        str << @commands[:moveto][col:1]
-        str << draw_subject(subj) if subj
-        str << draw_self(inst) if inst
+
+    def update_inplace subj,inst
+      goto = opts[:goto] || opts[:at] || @home
+      output :to_stdout do |str|
+        str << c.moveto(goto) 
+        update
       end
-      $stdout.print str
+      sleep @delay
       true
     end
-
-    def close
-      $stdout << @commands[:end]
+        
+    def update subj, inst, opts = {}
+      display
+      output :to_stdout do |str|
+        str << there_and_back do
+          str << c.mrcup(-(@size[:rows]-1),0)
+          str << c.moveto(col:1)
+          str << update_subject(subj) if subj
+          str << update_self(inst) if inst
+        end
+      end
+      true
     end
   end
 end
